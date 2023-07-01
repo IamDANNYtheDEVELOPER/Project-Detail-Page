89c66f8aedf9c8855b22d9593fb8b49ec30c69bb
Daniel Satapasti <dannythecodedeveloper@gmail.com>
2023-07-01 17:37:44 +0530

    some issue pending

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -21,7 +21,7 @@ navToggle.addEventListener('click', () =>
 
 
 
-// TESTING POP_UP
+// Designs Pop-Up
 
 var openBtn = document.getElementById("openBtn");
 var designsPopup = document.getElementById("designsPopup");
@@ -35,3 +35,32 @@ closeBtn.addEventListener("click", function () {
   designsPopup.style.display = "none";
 });
 
+
+// Designs Pop-Up - Filterbuttons selections
+
+const filterButtons = document.querySelectorAll(".filter-buttons .filterbutton");
+const filterableImages = document.querySelectorAll(".filterable-images .filterable-image");
+// console.log(filterButtons, filterableImages);
+
+
+
+//Pop-Up stopped responsing after the below code
+
+//Defining the filterButtons function
+const filterImages = e => {
+  document.querySelector(".active").classList.remove("active");
+  e.target.classList.add("active");
+  
+  //Iterate over each filterable image
+  filterableImages.forEach(filterable-image => {
+    //Add "hide" class to hide the image initially
+    filterable-image.classList.add("hide");
+    //Check if the image matches the selected filter or "all" is selected
+    if(filterable-image.dataset.name === e.target.dataset.name || e.target.dataset.name === "all" ){
+      filterable-image.classList.remove("hide");
+    }
+  });
+};
+
+// Adding Click event listner to each filter button
+filterButtons.forEach(.filterbutton => .filterbutton.addEventListener("click",))
